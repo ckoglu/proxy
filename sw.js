52647fe246c28c93a6c68787d985086d598b4e4f
ck <75999325+ckoglu@users.noreply.github.com>
2025-08-12 13:59:40 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  const pathname = location.pathname.split('/').filter(Boolean);
+  const repo = pathname.length > 0 ? `/${pathname[0]}/` : '/';
+  if (url.pathname === repo && url.searchParams.has("site")) {
+    const site = url.searchParams.get("site");
+    const proxyURL = `https://script.google.com/macros/s/AKfycbw_MYcInMCCYiQZNzeaZAp7Upl_UwNZS2O1rlx1bDBwBT7UFJJPEpvNSSmbkCgWXATk/exec?site=${encodeURIComponent(site)}`;
+    event.respondWith(
+      (async () => {
+        try {
+          const res = await fetch(proxyURL);
+          const html = await res.text();
+          return new Response(html, {headers: { "Content-Type": "text/plain; charset=utf-8", "Content-Disposition": "inline"}});
+        } catch (err) {
+          return new Response(JSON.stringify({ error: err.message }), {headers: { "Content-Type": "application/json; charset=utf-8" }, status: 500});
+        }
+      })()
+    );
+  }
+});
